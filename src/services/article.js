1f5e9ea4524efef820a7325bf161f297c1a8043a
hihassan1998 <hassanihussain1998@gmail.com>
2024-04-27 15:07:34 +0200

    ready for deployment

--- src/services/article.js
+++ src/services/article.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
 // NOT currently woring for rapidapi.com passed key in directlyinstead
-//const rapidApiKey = import.meta.env.VITE_MY_API_KEY
+const rapidApiKey = import.meta.env.VITE_MY_API_KEY
 
 // FOR the deployment of the app
 // const rapidApiKey = process.env.VITE_MY_API_KEY
@@ -12,7 +12,7 @@ export const articleApi = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', '82e0dce41emsh34e0d22e81aed3dp1cfebcjsndd8fc680f66b'); //rapidApiKey
+            headers.set('X-RapidAPI-Key', rapidApiKey); //rapidApiKey
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers
